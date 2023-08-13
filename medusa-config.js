6cfd5cb8778e03304a386313ccd5f7ee97246f5c
Farsky <farsky_vt@yahoo.com>
2023-08-13 09:49:58 +0700

    Change database config

--- medusa-config.js
+++ medusa-config.js
@@ -28,7 +28,7 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const POSTGRES_URL = process.env.POSTGRES_URL || "postgres://localhost/medusa-store";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
@@ -73,7 +73,7 @@ const projectConfig = {
   jwtSecret: process.env.JWT_SECRET,
   cookieSecret: process.env.COOKIE_SECRET,
   store_cors: STORE_CORS,
-  database_url: DATABASE_URL,
+  database_url: POSTGRES_URL,
   database_type: 'postgres',
   admin_cors: ADMIN_CORS,
   database_extra: process.env.NODE_ENV !== "development" ? { ssl: { rejectUnauthorized: false } } : {},
